63511f603e57c5d687a1408d880d29e67a657357
Farhad Hossain <tlcfarhad@gmail.com>
2019-01-13 02:13:54 +0600

    Login&Register

---
+++ app/src/main/java/com/example/farhadhossain/tongdemo2/User.java
@@ -0,0 +1,20 @@
+package com.example.farhadhossain.tongdemo2;
+
+/**
+ * Created by Farhad Hossain on 12-Jan-19.
+ */
+
+public class User {
+    public String id;
+    public String userName;
+    public String email;
+    public String password;
+
+    public User(String id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
